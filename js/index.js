8161108ac96b7b3c0927e531eb0a48207062e166
jamesonarnett <jamesonarnett@yahoo.com>
2022-10-10 14:43:02 -0500

    Resized tiled map, rendering to canvas

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
 canvas.width = 1024;
 canvas.height = 576;
 
-console.log(canvas);
+c.fillStyle = "white";
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+const image = new Image();
+image.src = "assets/imgs/pelletTown.png";
+image.onload = () => {
+  c.drawImage(image, -750, -500);
+};
